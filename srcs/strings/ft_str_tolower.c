89eac5a4685f49145f45341851cfd32890fd4771
Rémi Claire <reclaire@student.42mulhouse.fr>
2024-10-04 10:18:24 +0200

    fics

--- srcs/strings/ft_str_tolower.c
+++ srcs/strings/ft_str_tolower.c
@@ -6,7 +6,7 @@
 /*   By: reclaire <reclaire@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/11 23:58:00 by reclaire          #+#    #+#             */
-/*   Updated: 2024/09/03 03:11:09 by reclaire         ###   ########.fr       */
+/*   Updated: 2024/10/04 09:56:16 by reclaire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_str_tolower(string str)
 {
 	while (*str)
 	{
-		*str = ft_tolower(str);
+		*str = ft_tolower(*str);
 		str++;
 	}
 }
