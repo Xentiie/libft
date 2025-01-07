1af8d98d80a46c7d31687a504f46db8c81308940
Rémi Claire <reclaire@student.42mulhouse.fr>
2025-01-07 01:40:39 +0100

    Fixed duplicate 'parse_mode' function

--- srcs/io/file_private.h
+++ srcs/io/file_private.h
@@ -6,7 +6,7 @@
 /*   By: reclaire <reclaire@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 08:44:37 by reclaire          #+#    #+#             */
-/*   Updated: 2024/11/22 08:48:01 by reclaire         ###   ########.fr       */
+/*   Updated: 2024/12/29 16:00:01 by reclaire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,7 @@
 #if defined(FT_OS_WIN)
 #include <windows.h>
 #endif
+
+bool __parse_mode(const_string mode, bool *readable, bool *writeable, bool *append, bool *binary_mode);
 
 #endif
