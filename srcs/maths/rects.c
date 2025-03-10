8c2c8ad86b8952b66c025a4d58de03970effa0f5
Rémi Claire <reclaire@student.42mulhouse.fr>
2025-03-11 00:34:27 +0100

    rects clip

--- srcs/maths/rects.c
+++ srcs/maths/rects.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rectsize.c                                         :+:      :+:    :+:   */
+/*   rects.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: reclaire <reclaire@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 14:55:12 by reclaire          #+#    #+#             */
-/*   Updated: 2025/03/10 23:52:14 by reclaire         ###   ########.fr       */
+/*   Updated: 2025/03/11 00:07:08 by reclaire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,5 @@
 
 extern t_iv2 ft_irectsize(t_iv4 rect);
 extern t_v2 ft_rectsize(t_v4 rect);
+extern t_iv4 ft_clip_rect_rect(t_iv4 r1, t_iv4 r2);
+extern t_v4 ft_clip_rect_rect_flt(t_v4 r1, t_v4 r2);
