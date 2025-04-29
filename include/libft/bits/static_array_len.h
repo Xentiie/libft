6cc4cecde62a661cc706914b17fbdc7e4e29d0f2
Rémi Claire <reclaire@student.42mulhouse.fr>
2025-04-29 15:31:20 +0200

    Small fix

--- include/libft/bits/static_array_len.h
+++ include/libft/bits/static_array_len.h
@@ -6,13 +6,13 @@
 /*   By: reclaire <reclaire@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 20:44:24 by reclaire          #+#    #+#             */
-/*   Updated: 2025/04/15 01:47:23 by reclaire         ###   ########.fr       */
+/*   Updated: 2025/04/23 02:10:03 by reclaire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #if !defined(FT_BITS_STATIC_ARRAY_LEN_H)
 #define FT_BITS_STATIC_ARRAY_LEN_H
 
-#define statarray_len(x) (sizeof(x) / sizeof((x[0])))
+#define statarray_len(x) (sizeof(x) / sizeof((x)[0]))
 
 #endif
